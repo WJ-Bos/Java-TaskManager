e08eecf0feefa03e7bc7de3e2b6c4781b22f9388
WJ-Boshoff <wjbosdev@gmail.com>
2024-09-04 09:14:34 +0200

    Initial commit

---
+++ src/main/java/org/jd522/screens/AddTaskScreen.java
@@ -0,0 +1,9 @@
+package org.jd522.screens;
+
+import javax.swing.*;
+
+public class AddTaskScreen extends JFrame {
+    public AddTaskScreen() {
+        super("Add Task");
+    }
+}
---
+++ src/main/java/org/jd522/screens/MainScreen.java
@@ -0,0 +1,9 @@
+package org.jd522.screens;
+
+import javax.swing.*;
+
+public class MainScreen extends JFrame {
+    public MainScreen() {
+        super("Main Screen");
+    }
+}
