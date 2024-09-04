0f5ea68b2bff150cf3308b2f0543944eec6f3b67
WJ-Boshoff <wjbosdev@gmail.com>
2024-09-04 09:41:31 +0200

    Added db to file Structure

--- src/main/java/org/jd522/Main.java
+++ src/main/java/org/jd522/Main.java
@@ -1,7 +1,15 @@
 package org.jd522;
 
+import org.jd522.screens.MainScreen;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new MainScreen().setVisible(true);
+            }
+        });
     }
 }
--- src/main/java/org/jd522/screens/MainScreen.java
+++ src/main/java/org/jd522/screens/MainScreen.java
@@ -5,5 +5,16 @@ import javax.swing.*;
 public class MainScreen extends JFrame {
     public MainScreen() {
         super("Main Screen");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        setResizable(false);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        
+        AddComponentsToScreen();
+    }
+
+    private void AddComponentsToScreen() {
+
     }
 }
