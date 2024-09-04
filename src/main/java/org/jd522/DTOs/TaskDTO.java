98aff133075916d7e17072d05b1270223f5596d5
WJ-Boshoff <willemwors15@gmail.com>
2024-09-04 21:55:12 +0200

    Added the task DTO to map the entity to a DTO

---
+++ src/main/java/org/jd522/DTOs/TaskDTO.java
@@ -0,0 +1,69 @@
+package org.jd522.DTOs;
+
+
+/**
+ * @Author: WJ-Bos
+ * Basic DTO (Data Transfer Object) class for a single Task
+ * this is needed because we don't want to expose the database table
+ * and we use this to map the data from the database to the DTO
+ * the DTO is then used to pass the Data to the JTable
+ */
+
+
+public class TaskDTO {
+
+    private int id;
+    private String title;
+    private String description;
+    private String category;
+    private String status;
+
+
+    public TaskDTO(int id, String title, String description, String category, String status) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
