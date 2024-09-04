dd1fc2bc6f8468977517cc004069f2ffa505abdb
WJ-Boshoff <wjbosdev@gmail.com>
2024-09-04 09:58:12 +0200

    Added title lol

---
+++ src/main/java/org/jd522/Constants/ColorConstants.java
@@ -0,0 +1,7 @@
+package org.jd522.Constants;
+
+public class ColorConstants {
+    public static final String BLACK = "#000000";
+    public static final String PRIMARY_PURPLE = "#6200EE";
+    public static final String OFF_WHITE = "#FFFFe4";
+}
--- src/main/java/org/jd522/screens/MainScreen.java
+++ src/main/java/org/jd522/screens/MainScreen.java
@@ -1,6 +1,9 @@
 package org.jd522.screens;
 
+import org.jd522.Constants.ColorConstants;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class MainScreen extends JFrame {
     public MainScreen() {
@@ -8,13 +11,19 @@ public class MainScreen extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
         setResizable(false);
-        setSize(800, 600);
+        setSize(1000, 600);
         setLocationRelativeTo(null);
+        getContentPane().setBackground(Color.decode(ColorConstants.OFF_WHITE));
         
         AddComponentsToScreen();
     }
 
     private void AddComponentsToScreen() {
-
+        JLabel title = new JLabel("Task Management");
+        title.setForeground(Color.decode(ColorConstants.BLACK));
+        title.setFont(new Font("Arial", Font.BOLD, 22));
+        title.setSize(200,30);
+        title.setLocation(400,30);
+        add(title);
     }
 }
