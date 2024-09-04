c038350809d74b1e3448a2eb047cfcfb454400d5
WJ-Boshoff <willemwors15@gmail.com>
2024-09-04 19:33:53 +0200

    Added New colors and Completed the UI Skeleton

--- src/main/java/org/jd522/Constants/ColorConstants.java
+++ src/main/java/org/jd522/Constants/ColorConstants.java
@@ -2,6 +2,11 @@ package org.jd522.Constants;
 
 public class ColorConstants {
     public static final String BLACK = "#000000";
-    public static final String PRIMARY_PURPLE = "#6200EE";
+    public static final String DARK_GREY = "#191919";
+    public static final String OFF_RED = "#A04747";
     public static final String OFF_WHITE = "#FFFFe4";
+    public static final String ORANGE = "#D8A25E";
+    public static final String GREEN = "#008170";
+    public static final String GREEN_GREY = "#3F4E4F";
+    public static final String LIGHT_PURPLE = "#816797";
 }
--- src/main/java/org/jd522/screens/MainScreen.java
+++ src/main/java/org/jd522/screens/MainScreen.java
@@ -13,17 +13,102 @@ public class MainScreen extends JFrame {
         setResizable(false);
         setSize(1000, 600);
         setLocationRelativeTo(null);
-        getContentPane().setBackground(Color.decode(ColorConstants.OFF_WHITE));
+        getContentPane().setBackground(Color.decode(ColorConstants.DARK_GREY));
         
         AddComponentsToScreen();
     }
 
     private void AddComponentsToScreen() {
         JLabel title = new JLabel("Task Management");
-        title.setForeground(Color.decode(ColorConstants.BLACK));
-        title.setFont(new Font("Arial", Font.BOLD, 22));
-        title.setSize(200,30);
+        title.setForeground(Color.WHITE);
+        title.setFont(new Font("Verdana", Font.BOLD, 22));
+        title.setSize(230,30);
         title.setLocation(400,30);
         add(title);
+
+        JTextPane searchBar = new JTextPane();
+        searchBar.setSize(400,30);
+        searchBar.setLocation(60,80);
+        searchBar.setText("Search");
+        searchBar.setFont(new Font("Verdana", Font.PLAIN, 18));
+        searchBar.setBorder(BorderFactory.createLineBorder(Color.decode(ColorConstants.BLACK)));
+        add(searchBar);
+
+        JButton searchButton = new JButton("Search");
+        searchButton.setSize(100,30);
+        searchButton.setLocation(480,80);
+        searchButton.setFont(new Font("Verdana", Font.BOLD, 16));
+        searchButton.setBorder(BorderFactory.createLineBorder(Color.decode(ColorConstants.BLACK)));
+        searchButton.setBackground(Color.decode(ColorConstants.ORANGE));
+        searchButton.setForeground(Color.WHITE);
+        add(searchButton);
+
+        JTable table = new JTable();
+        table.setSize(520, 300);
+        table.setLocation(60, 130);
+        add(table);
+
+        JButton updateTaskButton = new JButton("Update Task");
+        updateTaskButton.setSize(200,50);
+        updateTaskButton.setLocation(100, 460);
+        updateTaskButton.setFont(new Font("Verdana", Font.BOLD, 16));
+        updateTaskButton.setBorder(BorderFactory.createLineBorder(Color.decode(ColorConstants.BLACK)));
+        updateTaskButton.setBackground(Color.decode(ColorConstants.ORANGE));
+        updateTaskButton.setForeground(Color.WHITE);
+        add(updateTaskButton);
+
+        JButton addTaskButton = new JButton("Add Task");
+        addTaskButton.setSize(200,50);
+        addTaskButton.setLocation(330, 460);
+        addTaskButton.setFont(new Font("Verdana", Font.BOLD, 16));
+        addTaskButton.setBorder(BorderFactory.createLineBorder(Color.decode(ColorConstants.BLACK)));
+        addTaskButton.setBackground(Color.decode(ColorConstants.GREEN));
+        addTaskButton.setForeground(Color.WHITE);
+        add(addTaskButton);
+
+        enum Category {
+            WORK,
+            PERSONAL,
+            LEARNING,
+            FITNESS
+        }
+
+        JLabel filterLabel = new JLabel("Search By Category");
+        filterLabel.setForeground(Color.WHITE);
+        filterLabel.setFont(new Font("Verdana", Font.BOLD, 16));
+        filterLabel.setSize(200,50);
+        filterLabel.setLocation(710, 120);
+        add(filterLabel);
+
+        JComboBox<String> comboBox = new JComboBox<>();
+        comboBox.setSize(230,50);
+        comboBox.setLocation(680, 180);
+        comboBox.setFont(new Font("Verdana", Font.PLAIN, 14));
+        comboBox.setBorder(BorderFactory.createLineBorder(Color.decode(ColorConstants.BLACK)));
+        comboBox.setBackground(Color.decode(ColorConstants.GREEN_GREY));
+        comboBox.setForeground(Color.WHITE);
+        add(comboBox);
+
+        for(Category category : Category.values()) {
+            comboBox.addItem(category.toString());
+        }
+
+        JButton filterButton = new JButton("Search Category");
+        filterButton.setSize(200,50);
+        filterButton.setLocation(695, 290);
+        filterButton.setFont(new Font("Verdana", Font.BOLD, 16));
+        filterButton.setBorder(BorderFactory.createLineBorder(Color.decode(ColorConstants.BLACK)));
+        filterButton.setBackground(Color.decode(ColorConstants.ORANGE));
+        filterButton.setForeground(Color.WHITE);
+        add(filterButton);
+
+        JButton exportButton = new JButton("Export CSV");
+        exportButton.setSize(200,50);
+        exportButton.setLocation(695, 350);
+        exportButton.setFont(new Font("Verdana", Font.BOLD, 16));
+        exportButton.setBorder(BorderFactory.createLineBorder(Color.decode(ColorConstants.BLACK)));
+        exportButton.setBackground(Color.decode(ColorConstants.LIGHT_PURPLE));
+        exportButton.setForeground(Color.WHITE);
+        add(exportButton);
     }
 }
