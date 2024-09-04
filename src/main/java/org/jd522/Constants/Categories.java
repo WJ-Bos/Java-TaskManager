a5a673e20a3f04a48f9fdb452f0c53da080ae54d
WJ-Boshoff <willemwors15@gmail.com>
2024-09-04 21:54:25 +0200

    Created Enums for Categories and Task status

---
+++ src/main/java/org/jd522/Constants/Categories.java
@@ -0,0 +1,13 @@
+package org.jd522.Constants;
+
+/**
+ * Using an Enum to store the categories
+ * Reason being to keep it simple and constant
+ */
+
+public enum Categories {
+    WORK,
+    PERSONAL,
+    LEARNING,
+    FITNESS
+}
---
+++ src/main/java/org/jd522/Constants/TaskStatus.java
@@ -0,0 +1,5 @@
+package org.jd522.Constants;
+
+public enum TaskStatus {
+    NOT_STARTED, IN_PROGRESS, COMPLETED
+}
