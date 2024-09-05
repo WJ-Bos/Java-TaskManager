16e4c6f9cbd90ea2b3b76d47642eb6364fcb0ca5
WJ-Boshoff <willemwors15@gmail.com>
2024-09-05 19:25:29 +0200

    Added new Colors

--- src/main/java/org/jd522/Constants/ColorConstants.java
+++ src/main/java/org/jd522/Constants/ColorConstants.java
@@ -4,7 +4,7 @@ public class ColorConstants {
     public static final String BLACK = "#000000";
     public static final String DARK_GREY = "#191919";
     public static final String OFF_RED = "#A04747";
-    public static final String OFF_WHITE = "#FFFFe4";
+    public static final String AQUA = "#1F6E8C";
     public static final String ORANGE = "#D8A25E";
     public static final String GREEN = "#008170";
     public static final String GREEN_GREY = "#3F4E4F";
