b935cf0e8ff2ecfb280e3911ef04edfffa5c667d
WJ-Boshoff <willemwors15@gmail.com>
2024-09-05 18:23:55 +0200

    Created Category class and used enum inside for the Category value.

---
+++ src/main/java/org/jd522/Classes/Category.java
@@ -0,0 +1,30 @@
+package org.jd522.Classes;
+
+public class Category {
+
+    private CategoryValue categoryValue;
+
+    public Category(CategoryValue categoryValue){
+        this.categoryValue = categoryValue;
+    }
+
+
+    public enum CategoryValue{
+        WORK("Work"),
+        PERSONAL("Personal"),
+        LEARNING("Learning"),
+        FITNESS("Fitness"),
+        OTHER("Other");
+
+        private final String label;
+
+        CategoryValue(String label){
+            this.label = label;
+        }
+
+        public String getLabel(){
+            return label;
+        }
+    }
+
+}
